06edbd85612a9705f64ed359ebe60f73994f2e1b
Hlawathage T.I.P <i21282904@my.sliit.lk>
2025-05-02 14:56:01 +0530

    made code efficient

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,22 +1,14 @@
 package org.example;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
-       ReverseWordString rw = new ReverseWordString();
-       System.out.println(rw.reverseWords("this      is   a    test"));
-=======
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ReverseWordString rw = new ReverseWordString();
+        System.out.println(rw.reverseWords("this    is   a  test     "));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        ProductOfArrayExeptSelf pas = new ProductOfArrayExeptSelf();
+        int[] nums = {1,3,4,5,16};
+        int[] arr = pas.productExceptSelf(nums);
+        for(int i =0;i< arr.length;i++){
+            System.out.print(arr[i]  +" ");
         }
->>>>>>> a0136517c4197ff1c2d574b1b6463b491dee7703
     }
 }
--- src/main/java/org/example/ProductOfArrayExeptSelf.java
+++ src/main/java/org/example/ProductOfArrayExeptSelf.java
@@ -1,22 +1,26 @@
 package org.example;
 
 public class ProductOfArrayExeptSelf {
-    class Solution {
-        public int[] productExceptSelf(int[] nums) {
-            int tray;
-            int[] arr = new int[nums.length];
-            for(int i = 0;i< nums.length; i++){
-                tray = nums[0];
-                nums[0] =nums[i];
-                nums[i]=tray;
-                arr[i]=1;
-                for(int j = 1;j< nums.length; j++){
-                    arr[i]=arr[i]*nums[j];
-                }
 
-            }
+        public int[] productExceptSelf(int[] nums) {
+            int[] arr = new int[nums.length];
+            int front;
+            int rear;
+
+
+                front = 1;
+                rear = 1;
+                for(int j = 0;j<nums.length;j++){
+                    arr[j] = front;
+                    front*= nums[j];
+                }
+                for(int j =nums.length-1;j>=0;j--){
+
+                    arr[j] *=rear;
+                    rear*= nums[j];
+                }
             return arr;
         }
     }
-}
+
 
