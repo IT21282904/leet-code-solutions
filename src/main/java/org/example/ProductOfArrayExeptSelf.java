c28513e011e3b195525be0b617af12182cd0eca6
Hlawathage T.I.P <i21282904@my.sliit.lk>
2025-05-02 12:03:49 +0530

    cant stage

--- src/main/java/org/example/ProductOfArrayExeptSelf.java
+++ src/main/java/org/example/ProductOfArrayExeptSelf.java
@@ -1,4 +1,22 @@
 package org.example;
 
 public class ProductOfArrayExeptSelf {
+    class Solution {
+        public int[] productExceptSelf(int[] nums) {
+            int tray;
+            int[] arr = new int[nums.length];
+            for(int i = 0;i< nums.length; i++){
+                tray = nums[0];
+                nums[0] =nums[i];
+                nums[i]=tray;
+                arr[i]=1;
+                for(int j = 1;j< nums.length; j++){
+                    arr[i]=arr[i]*nums[j];
+                }
+
+            }
+            return arr;
+        }
+    }
 }
+
